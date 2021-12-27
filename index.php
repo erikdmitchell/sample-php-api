0d0759c47e2315c6846b9768a489522536a1cb6a
Erik <erikdmitchell@gmail.com>
2021-12-27 12:20:33 -0500

    updated dir

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-require __DIR__ . "bootstrap.php";
- 
+require __DIR__ . "/bootstrap.php";
+
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode( '/', $uri );
  
@@ -9,8 +9,12 @@ if ((isset($uri[2]) && $uri[2] != 'user') || !isset($uri[3])) {
     exit();
 }
  
-require PROJECT_ROOT_PATH . "user-controller.php";
+require PROJECT_ROOT_PATH . "/user-controller.php";
  
 $objFeedController = new UserController();
 $strMethodName = $uri[3] . 'Action';
 $objFeedController->{$strMethodName}();
+
+
+// https://localhost/index.php/{MODULE_NAME}/{METHOD_NAME}?limit={LIMIT_VALUE}
+// http://localhost/index.php/user/list?limit=20
