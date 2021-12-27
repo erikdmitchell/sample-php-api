1ff48a82941509b78e5355ddbd44ed2da3103514
Erik <erikdmitchell@gmail.com>
2021-12-27 12:29:01 -0500

    em api files added

--- index.php
+++ index.php
@@ -18,3 +18,23 @@ $objFeedController->{$strMethodName}();
 
 // https://localhost/index.php/{MODULE_NAME}/{METHOD_NAME}?limit={LIMIT_VALUE}
 // http://localhost/index.php/user/list?limit=20
+
+
+/*
+require_once 'myapi.class.php';
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+// Requests from the same server don't have a HTTP_ORIGIN header
+if (!array_key_exists('HTTP_ORIGIN', $_SERVER)) {
+    $_SERVER['HTTP_ORIGIN'] = $_SERVER['SERVER_NAME'];
+}
+
+try {
+	$API = new MyAPI($_REQUEST['request'], $_SERVER['HTTP_ORIGIN']);
+    echo $API->processAPI();
+} catch (Exception $e) {
+    echo json_encode(Array('error' => $e->getMessage()));
+}
+*/
