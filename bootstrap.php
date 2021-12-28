0bda416c6cc5d53642bb2cdc3441613e78bf9f4d
Erik <erikdmitchell@gmail.com>
2021-12-27 22:04:33 -0500

    updated bootstrap file

--- bootstrap.php
+++ bootstrap.php
@@ -1,8 +1,7 @@
 <?php
-define("PROJECT_ROOT_PATH", __DIR__);
- 
-// include main configuration file
-//require_once PROJECT_ROOT_PATH . "/inc/config.php";
- 
-// include the base controller file
-require_once PROJECT_ROOT_PATH . "/base-controller.php";
+define("API_ROOT_PATH", __DIR__);
+
+// Connect to MySQL database.
+include_once( API_ROOT_PATH 'config/class-apidb.php' );
+
+// abstract/abstract-class-db.php
