0add45273271644a9343d71cb97a76b81774024d
Erik <erikdmitchell@gmail.com>
2021-12-28 22:00:20 -0500

    tweask api db

--- config/class-apidb.php
+++ config/class-apidb.php
@@ -1,54 +1,25 @@
 <?php 
 
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
 /**
  * DB class.
  */
 class APIDB {
 
-    /**
-     * connection
-     * 
-     * @var mixed
-     * @access protected
-     */
     protected $connection;
     
-	/**
-	 * query
-	 * 
-	 * @var mixed
-	 * @access protected
-	 */
 	protected $query;
-    
-    /**
-     * show_errors
-     * 
-     * (default value: TRUE)
-     * 
-     * @var mixed
-     * @access protected
-     */
-    protected $show_errors = TRUE;
-    
-    /**
-     * query_closed
-     * 
-     * (default value: TRUE)
-     * 
-     * @var mixed
-     * @access protected
-     */
-    protected $query_closed = TRUE;
-	
-	/**
-	 * query_count
-	 * 
-	 * (default value: 0)
-	 * 
-	 * @var int
-	 * @access public
-	 */
+
+    protected $show_errors = true;
+
+	public $insert_id = 0;    
+
+    //protected $query_closed = true;
+
 	public $query_count = 0;
     
 	/**
@@ -71,170 +42,7 @@ class APIDB {
 		
 		$this->connection->set_charset($charset);       
     }
-    
-    /**
-     * query function.
-     * 
-     * @access public
-     * @param mixed $query
-     * @return void
-     */
-    public function query($query) {
-        if (!$this->query_closed) {
-            $this->query->close();
-        }
-        
-		if ($this->query = $this->connection->prepare($query)) {
-            if (func_num_args() > 1) {
-                $x = func_get_args();
-                $args = array_slice($x, 1);
-				$types = '';
-                $args_ref = array();
-                foreach ($args as $k => &$arg) {
-					if (is_array($args[$k])) {
-						foreach ($args[$k] as $j => &$a) {
-							$types .= $this->_gettype($args[$k][$j]);
-							$args_ref[] = &$a;
-						}
-					} else {
-	                	$types .= $this->_gettype($args[$k]);
-	                    $args_ref[] = &$arg;
-					}
-                }
-				array_unshift($args_ref, $types);
-                call_user_func_array(array($this->query, 'bind_param'), $args_ref);
-            }
-            $this->query->execute();
-           	if ($this->query->errno) {
-				$this->error('Unable to process MySQL query (check your params) - ' . $this->query->error);
-           	}
-            $this->query_closed = FALSE;
-			$this->query_count++;
-        } else {
-            $this->error('Unable to prepare MySQL statement (check your syntax) - ' . $this->connection->error);
-        }
-		return $this;
-    }    	    
 
-	/**
-	 * fetchAll function.
-	 * 
-	 * @access public
-	 * @param mixed $callback (default: null)
-	 * @return void
-	 */
-	public function fetchAll($callback = null) {
-	    $params = array();
-        $row = array();
-	    $meta = $this->query->result_metadata();
-	    while ($field = $meta->fetch_field()) {
-	        $params[] = &$row[$field->name];
-	    }
-	    call_user_func_array(array($this->query, 'bind_result'), $params);
-        $result = array();
-        while ($this->query->fetch()) {
-            $r = array();
-            foreach ($row as $key => $val) {
-                $r[$key] = $val;
-            }
-            if ($callback != null && is_callable($callback)) {
-                $value = call_user_func($callback, $r);
-                if ($value == 'break') break;
-            } else {
-                $result[] = $r;
-            }
-        }
-        $this->query->close();
-        $this->query_closed = TRUE;
-		return $result;
-	}
-
-	/**
-	 * fetchArray function.
-	 * 
-	 * @access public
-	 * @return void
-	 */
-	public function fetchArray() {
-	    $params = array();
-        $row = array();
-	    $meta = $this->query->result_metadata();
-	    while ($field = $meta->fetch_field()) {
-	        $params[] = &$row[$field->name];
-	    }
-	    call_user_func_array(array($this->query, 'bind_result'), $params);
-        $result = array();
-		while ($this->query->fetch()) {
-			foreach ($row as $key => $val) {
-				$result[$key] = $val;
-			}
-		}
-        $this->query->close();
-        $this->query_closed = TRUE;
-		return $result;
-	}
-
-	/**
-	 * close function.
-	 * 
-	 * @access public
-	 * @return void
-	 */
-	public function close() {
-		return $this->connection->close();
-	}
-
-    /**
-     * numRows function.
-     * 
-     * @access public
-     * @return void
-     */
-    public function numRows() {
-		$this->query->store_result();
-		return $this->query->num_rows;
-	}
-
-	/**
-	 * affectedRows function.
-	 * 
-	 * @access public
-	 * @return void
-	 */
-	public function affectedRows() {
-		return $this->query->affected_rows;
-	}
-
-    /**
-     * lastInsertID function.
-     * 
-     * @access public
-     * @return void
-     */
-    public function lastInsertID() {
-    	return $this->connection->insert_id;
-    }
-
-    /**
-     * error function.
-     * 
-     * @access public
-     * @param mixed $error
-     * @return void
-     */
-    public function error($error) {
-        if ($this->show_errors) {
-            exit($error);
-        }
-    }
-
-	/**
-	 * _gettype function.
-	 * 
-	 * @access private
-	 * @param mixed $var
-	 * @return void
-	 */
 	private function _gettype($var) {
 	    if (is_string($var)) return 's';
 	    if (is_float($var)) return 'd';
@@ -242,4 +50,113 @@ class APIDB {
 	    return 'b';
 	}
 
+	public function _escape( $data ) {
+
+	}
+
+	public function query( $query ) {
+
+        $stmt = $this->connection->prepare( $query );
+        $stmt->execute($query);
+        
+        return $stmt;
+	}
+
+	public function insert( $table, $data, $format = null ) {
+        // Insert a row/s in a Database Table
+        public function Insert( $statement = "" , $parameters = [] ){
+            try{
+				
+                $this->executeStatement( $statement , $parameters );
+                return $this->connection->lastInsertId();
+				
+            }catch(Exception $e){
+                throw new Exception($e->getMessage());   
+            }		
+        }
+	}
+	
+        // Select a row/s in a Database Table
+        public function Select( $statement = "" , $parameters = [] ){
+            try{
+				
+                $stmt = $this->executeStatement( $statement , $parameters );
+                return $stmt->fetchAll();
+				
+            }catch(Exception $e){
+                throw new Exception($e->getMessage());   
+            }		
+        }		  	    	
+
+	public function get_var( $query = null, $x = 0, $y = 0 ) {
+
+	}
+
+	public function get_row( $query = null, $output = OBJECT, $y = 0 ) {
+
+	}
+
+	public function update( $table, $data, $where, $format = null, $where_format = null ) {
+        // Update a row/s in a Database Table
+        public function Update( $statement = "" , $parameters = [] ){
+            try{
+				
+                $this->executeStatement( $statement , $parameters );
+				
+            }catch(Exception $e){
+                throw new Exception($e->getMessage());   
+            }		
+        }	
+	}
+
+	public function delete( $table, $column, $value ) {
+    	return $this->query( "DELETE FROM $table WHERE $column = $value" );
+	}	
+
+    public function error($error) {
+        if ($this->show_errors) {
+            exit($error);
+        }
+    }			
+
 }
+
+
+
+class xyz {
+  /**
+  * Open the connection to your database.
+  */
+  function open() {
+    $this->connection = new mysqli($this->host, $this->username, $this->password, $this->database, $this->port);
+  }
+
+  /**
+  * Close the connection to your database.
+  */
+  function close() {
+    $this->connection->close();
+  }
+
+  /**
+  *
+  * Execute your query
+  *
+  * @param string $query - your sql query
+  * @return the result of the executed query 
+  */
+  function query($query) {
+    return $this->connection->query($query);
+  }
+
+  /**
+  * Escape your parameters to prevent SQL Injections! Usage: See documentation (link at the top of the file)
+  *
+  * @param string $string - your parameter to escape
+  * @return the escaped string 
+  */
+  function escape($string) {
+    return $this->connection->escape_string($string);
+  }
+}
+
