595cf50e5febb65bb4b144c243b991e2197f807b
Erik <erikdmitchell@gmail.com>
2021-12-30 16:37:03 -0500

    fixed error in file

--- bootstrap.php
+++ bootstrap.php
@@ -1,2 +1,9 @@
+<?php
+define("API_ROOT_PATH", __DIR__);
 
-Parse error: syntax error, unexpected token "public", expecting end of file in /Users/erikmitchell/vagrant-local/www/nonwp/public_html/api/vendor/squizlabs/php_codesniffer/src/Standards/PEAR/Tests/Commenting/FunctionCommentUnitTest.inc on line 380
+// Connect to MySQL database.
+include_once( API_ROOT_PATH . '/class-database.php' );
+
+// abstract/abstract-class-db.php
+
+include_once( API_ROOT_PATH . '/db-class.php' ); // db test class.
