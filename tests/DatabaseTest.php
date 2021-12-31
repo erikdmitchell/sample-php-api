2235f893fecc4b75e5d1490f15cbcdfc02b2d01d
Erik <erikdmitchell@gmail.com>
2021-12-30 21:58:58 -0500

    sample php unit test

--- tests/DatabaseTest.php
+++ tests/DatabaseTest.php
@@ -1,68 +1,32 @@
 <?php
-
-// MySQL XML DataSet
-
-// mysqldump --xml -t -u [username] --password=[password] [database] > /path/to/file.xml
-
+    
+// run tests agsainst db
+function createScoresTable($db_connection) {
+    $db_connection->query(
+        'DROP TABLE IF EXISTS game_scores'
+    );
+    
 /*
-use PHPUnit\Framework\TestCase;
-use PHPUnit\DbUnit\TestCaseTrait;
-
-class MyTestCase extends TestCase
-{
-    use TestCaseTrait;
-
-    public function getDataSet()
-    {
-        return $this->createMySQLXMLDataSet('/path/to/file.xml');
-    }
-}
+    $db_connection->query(
+        'CREATE TABLE game_scores (
+        id int(10) NOT NULL AUTO_INCREMENT,
+        name varchar(16) NOT NULL DEFAULT '',
+        score int(10) NOT NULL,
+        PRIMARY KEY (id)
+        ) ENGINE=InnoDB'
+    );
 */
 
-// guestbook test
-use PHPUnit\Framework\TestCase;
-use PHPUnit\DbUnit\TestCaseTrait;
- 
-class GuestBookTest extends TestCase{
-    use TestCaseTrait;
- 
-    // only instantiate pdo once for test clean-up/fixture load
-    static private $pdo = null;
- 
-    // only instantiate PHPUnit\DbUnit\Database\Connection once per test
-    private $conn = null;
- 
-    final public function getConnection() {
-        if ($this->conn === null) {
-            if (self::$pdo == null) {
-                self::$pdo = new PDO( $GLOBALS['DB_DSN'], $GLOBALS['DB_USER'], $GLOBALS['DB_PASSWD'] );
-            }
-            $this->conn = $this->createDefaultDBConnection(self::$pdo, $GLOBALS['DB_DBNAME']);
-        }
- 
-        return $this->conn;
-    }
- 
-    public function getDataSet() {
-        return $this->createFlatXmlDataSet('./tests/guestbook_fixture.xml');
-    }
- 
-    public function testRowCount() {
-        $this->assertSame(2, $this->getConnection()->getRowCount('guestbook'), "Pre-Condition");
-    }
- 
-    public function testAddGuest() {
- 
-        $guestbook = new GuestBook();
-        $guestbook->addGuest("George", "AP, India", "4545");
- 
-        $queryTable = $this->getConnection()->createQueryTable(
-            'guestbook', 'SELECT id, name, address, phone FROM guestbook'
-        );
- 
-        $expectedTable = $this->createFlatXmlDataSet("./tests/guestbook_expected.xml")->getTable("guestbook");
- 
-        $this->assertTablesEqual($expectedTable, $queryTable);
- 
-    }
+/*
+    $db_connection->query(
+        'INSERT INTO `game_scores` (`name`, `score`)
+           VALUES ("Matt", 20), ("Matt, 1200),
+           ("Matt", 2300), ("Kathleen", 6700),
+           ("Will", 6200), ("Will", 4800)'
+    );
+*/
+}
+
+function testHighScores() { 
+echo 'a';
 }
