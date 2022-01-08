0277774a796dc9167fb802cd5c3df145f02024ec
Erik <erikdmitchell@gmail.com>
2022-01-08 12:52:03 -0500

    updated parse args func

--- src/app/functions.php
+++ src/app/functions.php
@@ -6,25 +6,13 @@
  * @version 0.1.0
  */
 
-namespace Mitchell\API\App;
-
-/**
- * Parse array args.
- *
- * @access public
- * @param mixed $args array.
- * @param array $defaults (default: array()).
- * @return array
- */
 function parse_args( $args, $defaults = array() ) {
-    $parsed_args = '';
-
     if ( is_object( $args ) ) {
         $parsed_args = get_object_vars( $args );
     } elseif ( is_array( $args ) ) {
         $parsed_args =& $args;
     } else {
-        parse_args( $args, $parsed_args );
+        parse_str( $args, $parsed_args );
     }
 
     if ( is_array( $defaults ) && $defaults ) {
